85e51523c4561bf80bdc38a0ff4095b4f3e1086a
MarkChang <shea4a71@gmail.com>
2019-01-25 23:01:41 +0800

    Add movie model

--- Media/API/JSON Model/MKCMovieModel.h
+++ Media/API/JSON Model/MKCMovieModel.h
@@ -1,5 +1,5 @@
 //
-//  MKCSongModel.h
+//  MKCMovieModel.h
 //  Media
 //
 //  Created by MarkChang on 2019/1/25.
@@ -9,21 +9,23 @@
 #import <Foundation/Foundation.h>
 #import "JSONModel.h"
 
-@protocol MKCSongInfoModel;
+@protocol MKCMovieInfoModel;
 
-@interface MKCSongInfoModel : JSONModel
+@interface MKCMovieInfoModel : JSONModel
 
 @property (nonatomic, copy) NSString <Optional> *trackName;
 @property (nonatomic, copy) NSString <Optional> *artistName;
-@property (nonatomic, copy) NSString <Optional> *collectionName;
+@property (nonatomic, copy) NSString <Optional> *trackCensoredName;
 @property (nonatomic, copy) NSString <Optional> *trackTimeMillis;
+@property (nonatomic, copy) NSString <Optional> *longDescription;
 @property (nonatomic, copy) NSString <Optional> *trackViewUrl;
 
 @end
 
-@interface MKCSongModel : JSONModel
+@interface MKCMovieModel : JSONModel
 
 @property (nonatomic, copy) NSString <Optional> *resultCount;
-@property (nonatomic, copy) NSArray<MKCSongInfoModel *> <MKCSongInfoModel, Optional> *results;
+@property (nonatomic, copy) NSArray<MKCMovieInfoModel *> <MKCMovieInfoModel, Optional> *results;
 
 @end
+
---
+++ Media/API/JSON Model/MKCMovieModel.m
@@ -0,0 +1,17 @@
+//
+//  MKCMovieModel.m
+//  Media
+//
+//  Created by MarkChang on 2019/1/25.
+//  Copyright © 2019 MarkChang. All rights reserved.
+//
+
+#import "MKCMovieModel.h"
+
+@implementation MKCMovieInfoModel
+
+@end
+
+@implementation MKCMovieModel
+
+@end
