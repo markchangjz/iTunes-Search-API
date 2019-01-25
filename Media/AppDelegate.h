8f3ba888df5a3fc40a681b35a4644071df9ab016
MarkChang <shea4a71@gmail.com>
2019-01-25 21:20:46 +0800

    Initial Commit

---
+++ Media/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Media
+//
+//  Created by MarkChang on 2019/1/25.
+//  Copyright © 2019 MarkChang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
