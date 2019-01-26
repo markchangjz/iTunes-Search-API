a0ff39054dcd7e77c01f302e0776ead51efed249
MarkChang <shea4a71@gmail.com>
2019-01-26 14:03:37 +0800

    add basic cell

---
+++ Media/View/MediaSearchResultCell/MKCBasicTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  MKCBasicTableViewCell.h
+//  Media
+//
+//  Created by MarkChang on 2019/1/26.
+//  Copyright © 2019 MarkChang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MKCBasicTableViewCell : UITableViewCell
+
++ (NSString *)identifier;
+
+@end
---
+++ Media/View/MediaSearchResultCell/MKCBasicTableViewCell.m
@@ -0,0 +1,28 @@
+//
+//  MKCBasicTableViewCell.m
+//  Media
+//
+//  Created by MarkChang on 2019/1/26.
+//  Copyright © 2019 MarkChang. All rights reserved.
+//
+
+#import "MKCBasicTableViewCell.h"
+
+@implementation MKCBasicTableViewCell
+
++ (NSString *)identifier {
+	return NSStringFromClass(self);
+}
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
