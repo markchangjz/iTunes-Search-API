b6f50e27f91849757a14e59d14550b277e758b45
MarkChang <shea4a71@gmail.com>
2019-01-27 10:09:19 +0800

    bridge collected movies data.

--- Media/MKCCollectedMoviesViewController.m
+++ Media/MKCCollectedMoviesViewController.m
@@ -10,8 +10,13 @@
 #import "MKCRequestAPI.h"
 #import "MKCDataPersistence.h"
 #import "MKCJSONModel.h"
+#import "MKCMovieTableViewCell.h"
+#import "UIImageView+WebCache.h"
 
-@interface MKCCollectedMoviesViewController ()
+@interface MKCCollectedMoviesViewController () <UITableViewDelegate, UITableViewDataSource, MKCMovieTableViewCellDelegate>
+
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, copy) NSArray<MKCMovieInfoModel *> *movies;
 
 @end
 
@@ -24,6 +29,46 @@
 	[self lookupcollectMoviesInfo];
 }
 
+#pragma mark - UITableViewDelegate & UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+	return self.movies.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+	MKCMovieTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:MKCMovieTableViewCell.identifier forIndexPath:indexPath];
+	
+	MKCMovieInfoModel *movieInfo = self.movies[indexPath.row];
+	[cell.coverImageView sd_setImageWithURL:[NSURL URLWithString:movieInfo.imageUrl]];
+	cell.trackName = movieInfo.trackName;
+	cell.artistName = movieInfo.artistName;
+	cell.trackCensoredName = movieInfo.trackCensoredName;
+	cell.trackTimeMillis = movieInfo.trackTimeMillis;
+	cell.longDescription = movieInfo.longDescription;
+	cell.isCollected = [MKCDataPersistence hasCollectdMovieWithTrackId:movieInfo.trackId];
+	
+	cell.delegate = self;
+	cell.tag = indexPath.row;
+	
+	return cell;
+}
+
+#pragma mark - MKCMovieTableViewCellDelegate
+
+- (void)movieTableViewCell:(MKCMovieTableViewCell *)movieTableViewCell collectMovieAtIndex:(NSInteger)index {
+	
+	NSString *trackId = self.movies[index].trackId;
+	
+	if ([MKCDataPersistence hasCollectdMovieWithTrackId:trackId]) {
+		[MKCDataPersistence removeCollectedMovieWithTrackId:trackId];
+	} else {
+		[MKCDataPersistence collectMovieWithTrackId:trackId];
+	}
+	
+	NSIndexPath *indexPath = [NSIndexPath indexPathForRow:index inSection:0];
+	[self.tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationNone];
+}
+
 #pragma mark - Fetch API
 
 - (void)lookupcollectMoviesInfo {
@@ -34,26 +79,57 @@
 	}
 	
 	[[MKCRequestAPI sharedAPI] lookupWithTrackIds:trackIds successHandler:^(NSURLResponse *response, id responseObject) {
-		NSLog(@"qaz: %@", responseObject);
-		
 		NSError *error = nil;
 		MKCMovieModel *model = [[MKCMovieModel alloc] initWithDictionary:responseObject error:&error];
 		if (error) { // Parse JSON failed
 			return;
 		}
 		
-		NSLog(@"%@", model);
-		
+		self.movies = model.results;
 	} failureHandler:^(NSError *error) {
 		
 	}];
-	
 }
 
 #pragma mark - UI Layout
 
 - (void)configureView {
 	self.view.backgroundColor = [UIColor whiteColor];
+	
+	// table view
+	[self.view addSubview:self.tableView];
+	[self layoutTableView];
+	
+	[self.tableView registerClass:[MKCMovieTableViewCell class] forCellReuseIdentifier:MKCMovieTableViewCell.identifier];
+}
+
+- (void)layoutTableView {
+	NSArray *tableViewHorizontalConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[tableView]-0-|" options:0 metrics:nil views:@{@"tableView": self.tableView}];
+	NSArray *tableViewVerticalConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[tableView]-0-|" options:0 metrics:nil views:@{@"tableView": self.tableView}];
+	[self.view addConstraints:tableViewHorizontalConstraints];
+	[self.view addConstraints:tableViewVerticalConstraints];
+}
+
+#pragma mark - binding
+
+- (void)setMovies:(NSArray<MKCMovieInfoModel *> *)movies {
+	_movies = movies;
+	
+	[self.tableView reloadData];
+}
+
+#pragma mark - lazy instance
+
+- (UITableView *)tableView {
+	if (!_tableView) {
+		_tableView = [[UITableView alloc] init];
+		_tableView.translatesAutoresizingMaskIntoConstraints = NO;
+		_tableView.delegate = self;
+		_tableView.dataSource = self;
+		_tableView.estimatedRowHeight = 200.0;
+		_tableView.rowHeight = UITableViewAutomaticDimension;
+	}
+	return _tableView;
 }
 
 @end
