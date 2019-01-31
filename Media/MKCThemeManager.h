223eae16302955fe92bbcb5d4b92d4e195c88356
MarkChang <shea4a71@gmail.com>
2019-01-31 16:17:34 +0800

    add theme manager

---
+++ Media/MKCThemeManager.h
@@ -0,0 +1,20 @@
+//
+//  MKCThemeManager.h
+//  Media
+//
+//  Created by MarkChang on 2019/1/31.
+//  Copyright © 2019 MarkChang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MKCDataPersistence.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MKCThemeManager : NSObject
+
++(void)applyTheme:(MKCTheme)theme;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Media/MKCThemeManager.m
@@ -0,0 +1,37 @@
+//
+//  MKCThemeManager.m
+//  Media
+//
+//  Created by MarkChang on 2019/1/31.
+//  Copyright © 2019 MarkChang. All rights reserved.
+//
+
+#import "MKCThemeManager.h"
+#import <UIKit/UIKit.h>
+
+@implementation MKCThemeManager
+
++(void)applyTheme:(MKCTheme)theme {
+	switch (theme) {
+		case MKCThemeLight:
+			[self applyLightTheme];
+			break;
+		case MKCThemeDark:
+			[self applyDarkTheme];
+			break;
+	}
+}
+
++ (void)applyLightTheme {
+	[[UITabBar appearance] setBarStyle:UIBarStyleDefault];
+	[[UITabBar appearance] setUnselectedItemTintColor:[UIColor lightGrayColor]];
+	[[UITabBar appearance] setTintColor:[UIColor blackColor]];
+}
+
++ (void)applyDarkTheme {
+	[[UITabBar appearance] setBarStyle:UIBarStyleBlack];
+	[[UITabBar appearance] setUnselectedItemTintColor:[UIColor lightGrayColor]];
+	[[UITabBar appearance] setTintColor:[UIColor whiteColor]];
+}
+
+@end
